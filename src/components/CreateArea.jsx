c20c7eaf17e48e6823b4c44c68b8de503a742520
Utkarshsingh30112002 <131717335+Utkarshsingh30112002@users.noreply.github.com>
2023-09-23 15:49:37 +0530

    Initial commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+  const [Click, Chang] = useState(false);
+  function change() {
+    Chang(!Click);
+  }
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {Click && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={Click ? 3 : 1}
+          onClick={change}
+        />
+        <Zoom in={Click}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+//createRoot.render(<App />, document.getElementById("root"));
+
+//This is an react appp
+//trying to clone google keeper
+// database not connected
+
+//Utkarsh Singh
+//created for educational purpose
